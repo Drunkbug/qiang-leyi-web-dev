ccbfc03379c1b27d1f3c94dcecc94385152f40f1
Drunkbug <leyiqiang@gmail.com>
2016-05-17 17:29:00 -0400

    finally...

---
+++ public/init.js
@@ -0,0 +1,10 @@
+/**
+ * Created by leyiqiang on 5/17/16.
+ */
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
