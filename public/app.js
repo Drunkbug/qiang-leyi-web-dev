993dd68ae60098ff1319a811b6321379a6d464bb
Drunkbug <leyiqiang@gmail.com>
2016-05-27 14:03:19 -0400

    applied IIFE

--- public/app.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by leyiqiang on 5/23/16.
- */
-
-(function(){
-    angular.module("WebAppMaker", []);
-})();
---
+++ public/assignment/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by leyiqiang on 5/27/16.
+ */
+(function (){
+    angular.module("WebAppMaker", [])
+})();// the second parenthesis execute the function immediately
+     // anything inside it gonna be protected
