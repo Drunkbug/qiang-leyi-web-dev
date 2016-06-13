af1de575604956631189881a48154efd8cd5f2cd
Drunkbug <leyiqiang@gmail.com>
2016-06-12 21:50:05 -0400

    add openshift database config

--- assignment/models/models.js
+++ assignment/models/models.js
@@ -2,8 +2,23 @@
  * Created by leyiqiang on 6/6/16.
  */
 module.exports = function () {
+
     var mongoose = require('mongoose');
-    mongoose.connect('mongodb://localhost/assignment');
+    var connectionString = 'mongodb://localhost/assignment';
+
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+
+    mongoose.connect(connectionString);
+
+
+    // var mongoose = require('mongoose');
+    // mongoose.connect('mongodb://localhost/assignment');
     var userModel = require("./user/user.model.server.js")();
     var websiteModel = require("./website/website.model.server.js")();
     var pageModel = require("./page/page.model.server.js")();
